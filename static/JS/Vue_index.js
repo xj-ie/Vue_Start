b6e1706786c5915816f0885b0af0b3c673cddd81
ie <273817691@qq.com>
2020-08-24 16:07:36 +0800

    父组件给子组件传值

--- static/JS/Vue_index.js
+++ static/JS/Vue_index.js
@@ -1,16 +1,17 @@
 //局部组件定义
 let zujian_a = {
-	template:'<div>局部组件定义{{age}}</div>',
+	template:'<div>局部组件定义{{age}}{{pro}}</div>',
 	data:()=>{
 		return {age:19}
-	}
+	},
+	props:['pro']
 };
 // 全局组定义
 Vue.component(
 	"component_all",
 	// 全局组件名称
 	{
-		template:'<div>全局组定义 <zujian_a></zujian_a>{{epwq}}</div>',
+		template:'<div>全局组定义 <zujian_a :pro="epwq"></zujian_a>{{epwq}}</div>',
 		components:{
 			zujian_a
 		},
