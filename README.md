371301cbaa734af3cd1b3317ca46a87c1c641820
ie <273817691@qq.com>
2020-08-24 11:27:28 +0800

    局部组件使用

---
+++ README.md
@@ -0,0 +1,9 @@
+### Vue全局组件效果：
+
+![image-20200824104357819](static\image\image-20200824104357819.png)
+
+![image-20200824104559187](static\image\image-20200824104559187.png)
+
+## 局部定义效果：
+
+![image-20200824112532902](static\image\image-20200824112532902.png)
--- static/JS/Vue_index.js
+++ static/JS/Vue_index.js
@@ -1,9 +1,18 @@
+//局部组件定义
+let zujian_a = {
+	template:'<div>局部组件定义</div>',};
 // 全局组定义
 Vue.component(
 	"component_all",
 	// 全局组件名称
 	{
-		template:'<div>全局组定义</div>'
+		template:'<div>全局组定义 <zujian_a></zujian_a>{{epwq}}</div>',
+		components:{
+			zujian_a
+		},
+		data:()=>{
+			return {epwq:'g an g an'}
+		}
 	}
 	)
 let binge = new Vue({
